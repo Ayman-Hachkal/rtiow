8c7797dd515c31141f086e988962d59c0f8a0d85
Ayman <Ayman.hachkal@gmail.com>
2026-01-10 23:17:25 +0000

    did chapter 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,25 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream> 
 
 int main() {
 
+  // Image properties
+
   int image_width = 256;
   int image_height = 256;
 
+  // Render
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
   for (int j {}; j < image_height; j++) {
     std::clog << "\rScanlines remaining " << (image_height - j) << ' ' << std::flush;
     for (int i {}; i <image_width; i++) {
-      double r = double(i) / (image_width-1);
-      double g = double(j) / (image_height-1);
-      double b = 0.0;
-
-      int ir = int(255.999 * r);
-      int ig = int(255.999 * g);
-      int ib = int(255.999 * b);
-
-      std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+      auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+      write_color(std::cout, pixel_color);
     }
   }
   std::clog << "\rDone.                  \n";
 }
+
