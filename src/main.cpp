0af1388f580c102db6fb5555b540e5b989137cd4
Ayman-Hachkal <ayman.hachkal@gmail.com>
2026-01-21 17:04:16 +0400

    finished chapter 6

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,31 @@
 #include "ray.h"
 #include "vec3.h"
 
+#include <cmath>
 #include <iostream> 
 
+double hit_sphere(const point3& center, double radius, const ray& r) {
+  vec3 oc = center - r.origin();
+  auto a = r.direction().length_squared(); 
+  auto h = dot(r.direction(), oc);
+  auto c = oc.length_squared() - radius * radius;
+  double discriminent = h*h - a*c;
+  if (discriminent < 0) {
+    return -1.0;
+  }
+  else {
+    return (h - std::sqrt(discriminent)) / a;
+  }
+
+}
+
 color ray_color(const ray& r) {
+  auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+  if (t > 0.0) {
+    vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+    return 0.5*(color(N.x()+1, N.y()+1, N.z()+1));
+  }
+
   vec3 unit_direction = unit_vector(r.direction());
   auto a = 0.5*(unit_direction.y()+1.0); 
   return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
@@ -15,7 +37,7 @@ int main() {
 
   // Image properties
   auto aspect_ratio = 16.0 / 9.0; 
-  int image_width = 400;
+  int image_width = 1920;
 
   int image_height = int(image_width / aspect_ratio);
   image_height = (image_height < 1) ? 1 : image_height;
