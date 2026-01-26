7c5839bfcd3e6282738d2622e5852270ad14ee71
Ayman-Hachkal <ayman.hachkal@gmail.com>
2026-01-26 16:48:34 +0400

    added a common header

--- src/sphere.h
+++ src/sphere.h
@@ -1,7 +1,7 @@
 #pragma once
 
+#include "rtweekend.h"
 #include "hittable.h"
-#include "vec3.h"
 
 class sphere : public hittable {
   public: 
@@ -31,7 +31,8 @@ class sphere : public hittable {
 
       rec.t = root;
       rec.p = r.at(rec.t);
-      rec.normal = (rec.p - center) / radius;
+      vec3 outward_normal = (rec.p - center) / radius;
+      rec.set_face_normal(r, outward_normal); 
 
       return true;
     }
