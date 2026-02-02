bf38ac41131d6dcaba57cf1ff9df481c4dbd149b
Ayman-Hachkal <ayman.hachkal@gmail.com>
2026-02-02 16:38:35 +0400

    Completed Chapter 9 Diffuse lighting

--- src/headers/camera.h
+++ src/headers/camera.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "headers/color.h"
+#include "headers/vec3.h"
 #include "hittable.h" 
 
 class camera {
@@ -7,6 +9,7 @@ class camera {
     double aspect_ratio = 1.0; 
     int image_width = 100;
     int samples_per_pixel = 10;
+    int max_depth = 10;
 
     void render(const hittable& world) {
       initialize();
@@ -18,7 +21,7 @@ class camera {
           color pixel_color(0, 0, 0); 
           for (int samples = 0; samples < samples_per_pixel; samples++) {
             ray r = get_ray(i, j);
-            pixel_color += ray_color(r, world);
+            pixel_color += ray_color(r, max_depth, world);
           }
           write_color(std::cout, pixel_samples_scale * pixel_color);
         }
@@ -74,10 +77,15 @@ class camera {
       return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
-    color ray_color(const ray& r, const hittable& world) const {
+    color ray_color(const ray& r,int depth, const hittable& world) const {
+      if(depth <= 0)
+        return color(0, 0, 0);
+
       hit_record rec;
-      if (world.hit(r, interval(0, infinity), rec)) {
-        return 0.5* (rec.normal + color(1,1,1)); 
+
+      if (world.hit(r, interval(0.001, infinity), rec)) {
+        vec3 direction = rec.normal + random_unit_vector();
+        return 0.5 * ray_color(ray(rec.p, direction), --depth, world); 
       }
 
       vec3 unit_direction = unit_vector(r.direction());
